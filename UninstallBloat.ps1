6a865727b0a7fd1beb75c9140c120ecaedd0282d
TankedThomas <TankedThomas@users.noreply.github.com>
2025-10-11 05:06:38 +1300

    Add script to remove pre-installed UWP apps

---
+++ UninstallBloat.ps1
@@ -0,0 +1,20 @@
+winget uninstall 9MSSGKG348SP	# Widgets/Web Experience
+winget uninstall 9MV0B5HZVK9Z	# Xbox
+winget uninstall 9NBLGGH4QGHW	# Sticky Notes
+winget uninstall 9NBLGGH4R32N	# Feedback Hub
+winget uninstall 9NBLGGH5R558	# To Do
+winget uninstall 9NHT9RB2F4HD	# Copilot
+winget uninstall 9NRX63209R7B	# Outlook
+winget uninstall 9P1J8S7CCWWT	# Clipchamp
+winget uninstall 9P7BP5VNWKX5	# Quick Assist
+winget uninstall 9WZDNCRFHVFW	# News
+winget uninstall 9WZDNCRFJ3P2	# Films & TV
+winget uninstall 9WZDNCRFJ3PT	# Media Player
+winget uninstall 9WZDNCRFJ3Q2	# Weather
+winget uninstall XP8BT8DW290MPQ	# Teams
+winget uninstall XPFFTQ037JWMHS	# Edge
+
+Get-AppxPackage *DevHome* | Remove-AppxPackage # Dev Home
+Get-AppxPackage *GetHelp* | Remove-AppxPackage # Get Help
+Get-AppxPackage *XboxGamingOverlay* | Remove-AppxPackage # Game Bar
+Get-AppxPackage *YourPhone* | Remove-AppxPackage # Phone Link
